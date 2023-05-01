653929670d709bddd270e4d3ec80d987fccb6c54
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-05-01 16:25:31 +0900

    EditFormをクラスコンポーネントへ変更・ユーザーの入力を常にTodoAppのstateへ反映するよう変更
    ・TodoListと同様に、関数コンポーネントである必要がなかったため、TodoAppと統一した。
    ・編集機能を実装する前段階として、親であるTodoAppのstateとバインディングした。

--- todo.js
+++ todo.js
@@ -2,13 +2,19 @@ class TodoApp extends React.Component {
   constructor (props) {
     super(props)
     const currentTodos = this.getTodosFromLocalStrage()
-    this.state = { newTodo: '', todos: currentTodos }
+    this.state = { newTodo: '', changedTodo: '', todos: currentTodos }
     this.handleChange = this.handleChange.bind(this)
+    this.handleChangeEdit = this.handleChangeEdit.bind(this)
     this.addTodo = this.addTodo.bind(this)
+    this.updateTodo = this.updateTodo.bind(this)
   }
 
   handleChange (text) {
     this.setState({ newTodo: text })
+  }
+
+  handleChangeEdit (text) {
+    this.setState({ changedTodo: text })
   }
 
   getTodosFromLocalStrage () {
@@ -31,12 +37,25 @@ class TodoApp extends React.Component {
     this.setState({ newTodo: '' })
   }
 
+  updateTodo (e) {
+    console.log('update!!!!')
+    console.log(e)
+    // e.preventDefault()
+    // if (this.state.changedTodo === '') return
+    // const todos = this.state.todos
+    // todos.push(this.state.newTodo)
+    // const json = JSON.stringify(todos, undefined, 0)
+    // localStorage.setItem('todoList', json)
+    // this.setState({ newTodo: '' })
+  }
+
   render () {
     return (
       <div>
         <CreateForm newTodo={this.state.newTodo} onNewTodoChange={this.handleChange} addTodo={this.addTodo}/>
         <p>TodoAppの中からのnewTodo: {this.state.newTodo}</p>
-        <EditForm />
+        <EditForm changedTodo={this.state.changedTodo} onTodoChange={this.handleChangeEdit} updateTodo={this.updateTodo} />
+        <p>TodoAppの中からのchangedTodo: {this.state.changedTodo}</p>
         <TodoList todos={this.state.todos}/>
       </div>
     )
@@ -55,29 +74,38 @@ class CreateForm extends React.Component {
 
   render () {
     return (
-    <div>
-      <label htmlFor="">
-        新規登録:
-      </label>
-      <form>
+      <div>
+        <label htmlFor="">
+          新規登録:
+        </label>
         <input type="text" value={this.props.newTodo} onChange={this.handleChange}/>
         <button type="submit" onClick={this.props.addTodo}>Add</button>
-      </form>
-    </div>
+      </div>
     )
   }
 }
 
-function EditForm (props) {
-  return (
-    <div>
-      <label htmlFor="">
-        更新:
-      </label>
-      <input type="text" />
-      <button>Edit</button>
-    </div>
-  )
+class EditForm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    this.props.onTodoChange(e.target.value)
+  }
+
+  render () {
+    return (
+      <div>
+        <label htmlFor="">
+          更新:
+        </label>
+        <input type="text" value={this.props.changedTodo} onChange={this.handleChange}/>
+        <button type="submit" onClick={this.props.updateTodo}>Edit</button>
+      </div>
+    )
+  }
 }
 
 class TodoList extends React.Component {
