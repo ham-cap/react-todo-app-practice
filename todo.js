169da3ff7ca30bc63522611c942cbeb15b346293
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-05-01 15:24:04 +0900

    新規登録機能を修正
    ・これまではCreateFormのstateにnewTodoとしてユーザーの入力した文字列を保持し、フォームの送信時にTodoAppのaddTodo関数へ渡していたが、CreateFormがそれを保持する必要がないため、newTodoをTodoAppのstateとして配置し直した。

--- todo.js
+++ todo.js
@@ -1,47 +1,14 @@
-function CreateForm (props) {
-  return (
-    <div>
-      <label htmlFor="">
-        新規登録:
-      </label>
-      <form onSubmit={props.addTodo}>
-        <input type="text" />
-        <button type="submit">Add</button>
-      </form>
-    </div>
-  )
-}
-
-function EditForm (props) {
-  return (
-    <div>
-      <label htmlFor="">
-        更新:
-      </label>
-      <input type="text" />
-      <button>Edit</button>
-    </div>
-  )
-}
-
-// function TodoList (props) {
-//   const todos = props.todos
-//   const todoList = todos.map((todo, index) =>
-//     <li key={index}>{todo}</li>
-//   )
-//   return (
-//     <ul>{todoList}</ul>
-//   )
-// }
-
 class TodoApp extends React.Component {
   constructor (props) {
     super(props)
-    this.addTodo = this.addTodo.bind(this)
-    console.log(this.getTodosFromLocalStrage())
     const currentTodos = this.getTodosFromLocalStrage()
-    this.state = { todos: currentTodos }
-    console.log(this)
+    this.state = { newTodo: '', todos: currentTodos }
+    this.handleChange = this.handleChange.bind(this)
+    this.addTodo = this.addTodo.bind(this)
+  }
+
+  handleChange (text) {
+    this.setState({ newTodo: text })
   }
 
   getTodosFromLocalStrage () {
@@ -56,23 +23,71 @@ class TodoApp extends React.Component {
 
   addTodo (e) {
     e.preventDefault()
-    console.log(e)
-    if (e === '') return
+    if (this.state.newTodo === '') return
     const todos = this.state.todos
-    todos.push(e.target[0].value)
+    todos.push(this.state.newTodo)
     const json = JSON.stringify(todos, undefined, 0)
     localStorage.setItem('todoList', json)
+    this.setState({ newTodo: '' })
   }
 
   render () {
     return (
       <div>
-        <CreateForm addTodo={this.addTodo} />
-        <p>{this.state.todos}</p>
+        <CreateForm newTodo={this.state.newTodo} onNewTodoChange={this.handleChange} addTodo={this.addTodo}/>
+        <p>TodoAppの中からのnewTodo: {this.state.newTodo}</p>
         <EditForm />
+        <TodoList todos={this.state.todos}/>
       </div>
     )
   }
+}
+
+class CreateForm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    this.props.onNewTodoChange(e.target.value)
+  }
+
+  render () {
+    return (
+    <div>
+      <label htmlFor="">
+        新規登録:
+      </label>
+      <form>
+        <input type="text" value={this.props.newTodo} onChange={this.handleChange}/>
+        <button type="submit" onClick={this.props.addTodo}>Add</button>
+      </form>
+    </div>
+    )
+  }
+}
+
+function EditForm (props) {
+  return (
+    <div>
+      <label htmlFor="">
+        更新:
+      </label>
+      <input type="text" />
+      <button>Edit</button>
+    </div>
+  )
+}
+
+function TodoList (props) {
+  const todos = props.todos
+  const todoList = todos.map((todo, index) =>
+    <li key={index}>{todo}</li>
+  )
+  return (
+    <ul>{todoList}</ul>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
